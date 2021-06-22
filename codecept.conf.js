dffac807b2ae9bab473278469d0f97cbf1647774
Javier <javier.fuentesm@hotmail.com>
2021-06-22 05:25:55 -0500

    WIP

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,41 +1,45 @@
 exports.config = {
-	output: './output',
-	helpers: {
-		WebDriver: {
-			url: 'https://int.providamx.metlife.com/ProvidaWeb',
-			browser: 'chrome',
-		},
-	},
-	include: {
-		I: './steps_file.js',
-		loginPage: './pages/loginPage.js',
-	},
-	mocha: {},
-	bootstrap: null,
-	teardown: null,
-	hooks: [],
-	plugins: {
-		screenshotOnFail: {
-			enabled: true,
-		},
-		pauseOnFail: {},
-		retryFailedStep: {
-			enabled: true,
-		},
-		tryTo: {
-			enabled: true,
-		},
-		allure: {},
-		selenoid: {
-			enabled: true,
-			deletePassed: false,
-			autoCreate: true,
-			autoStart: true,
-			sessionTimeout: '30m',
-			enableVideo: true,
-			enableLog: true,
-		},
-	},
-	tests: './tests/*_test.js',
-	name: 'Webdriver-video',
+  output: './output',
+  helpers: {
+    WebDriver: {
+      url: 'https://www.fcs-monitoreo.com/home.html',
+      browser: 'chrome'
+    }
+  },
+  include: {
+    I: './steps_file.js',
+    loginPage: './pages/loginPage.js'
+  },
+  mocha: {},
+  bootstrap: null,
+  teardown: null,
+  hooks: [],
+  gherkin: {
+    features: './features/*.feature',
+    steps: ['./step_definitions/steps.js']
+  },
+  plugins: {
+    screenshotOnFail: {
+      enabled: true
+    },
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    allure: {},
+    selenoid: {
+      enabled: true,
+      deletePassed: false,
+      autoCreate: true,
+      autoStart: true,
+      sessionTimeout: '30m',
+      enableVideo: true,
+      enableLog: true
+    }
+  },
+  tests: './tests/*_test.js',
+  name: 'Webdriver-video'
 }
---
+++ step_definitions/registroSteps.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+const { I } = inject();
+// Add in your custom step files
+
+// eslint-disable-next-line no-undef
+Given('I have a defined step', () => {
+    // TODO: replace with your own step
+});
