437cb4113a8b728fb0612f31ce82710882f070c2
Javier <javier.fuentesm@hotmail.com>
2021-06-23 20:39:30 -0400

    se mejora el wait

--- step_definitions/steps.js
+++ step_definitions/steps.js
@@ -1,10 +1,9 @@
 /* eslint-disable no-undef */
 const { I } = inject()
-// Add in your custom step files
 
 Given('Im logged in', async () => {
 	I.amOnPage('/')
-	I.wait(10)
+	I.waitToHide("Cargando",10)
 	const amNotILoggedIn = await tryTo(() => I.see('MONITOR FCS'))
 	if (amNotILoggedIn) {
 		I.fillField('emailLogin', 's_fuentesrj@globalhitss.com')
